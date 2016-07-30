680795ba89e441a49465cf54ebb0811ab3a2dfc7
kimown <kimown@163.com>
2016-07-30 12:16:57 +0800

    clear

--- themes/auto-update-theme.js
+++ themes/auto-update-theme.js
@@ -18,22 +18,22 @@ var cprocess;
 // https://git.oschina.net/kimown/ExtJS.git
 // https://github.com/hexojs/hexo-theme-landscape.git
 // https://git.oschina.net/kimown/hexo-theme-landscape.git
-const themeGitUrl = 'https://git.oschina.net/kimown/hexo-theme-landscape.git';
+const themeGitUrl = 'https://github.com/hexojs/hexo-theme-landscape.git';
 const themeDirName = themeGitUrl.match(/\/\/(.*)\/(.*)\/(.*)\.git/)[3];
 
 const commandList = [
     `rm -rf  ${themeGitUrl} ${themeDirName}-tmp`,
     'git commit -m powered_by_program_delete_tmp_files',
     'git push origin master',
-    //`git clone ${themeGitUrl} ${themeDirName}-tmp`,
-    //`git add ${themeDirName}-tmp/*`,
-    //'git commit -m powered_by_program_commit_tmp_files',
-    //'git push origin master',
-    //{
-    //    modulePath: path.join(__dirname, 'auto-update-theme-fs.js'),
-    //    execArgv: ['--harmony_destructuring', '--harmony_array_includes'],
-    //    args: `${themeDirName}-tmp`
-    //}
+    `git clone ${themeGitUrl} ${themeDirName}-tmp`,
+    `git add ${themeDirName}-tmp/*`,
+    'git commit -m powered_by_program_commit_tmp_files',
+    'git push origin master',
+    {
+        modulePath: path.join(__dirname, 'auto-update-theme-fs.js'),
+        execArgv: ['--harmony_destructuring', '--harmony_array_includes'],
+        args: `${themeDirName}-tmp`
+    }
 
 ];
 
