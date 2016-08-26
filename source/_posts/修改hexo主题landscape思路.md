4aeeec8ccbda645bf25e9effb1133e709e7d2d4c
kimown <kimown@163.com>
2016-08-26 21:46:03 +0800

    add : theme

---
+++ source/_posts/修改hexo主题landscape思路.md
@@ -0,0 +1,70 @@
+---
+title: 修改hexo主题landscape思路
+date: 2016-08-24 20:07:36
+tags: Node.js
+---
+
+ In all：描述下修改主题landscape文件的思路
+
+
+ <!-- more -->
+
+ 和有后台的web不一样，静态博客的速度取决于网络、Github Pages托管的静态文件的加载速度，记得有个经典的题目关于在浏览器中输入url到呈现网页过程中发生了什么，quora上有一个[回答](https://www.quora.com/What-are-the-series-of-steps-that-happen-when-an-URL-is-requested-from-the-address-field-of-a-browser),大致流程都描述了出来,还有一篇更详细的[文章](http://fex.baidu.com/blog/2014/05/what-happen/) (擦，居然看到了R大)
+
+
+
+ 那么，按照quora的回答，优化静态博客的一些思路就显而易见了
+ - url尽可能短
+ - 预缓存dns查询结果，或者直接将修改hosts将域名和ip对应(本机环境)
+ - 如果不考虑到可读性，html 、js、css 等静态资源进行压缩、合并处理
+ - 考虑到[浏览器并发问题](https://www.zhihu.com/question/20474326),将和博客主体无关的其他资源放置在cdn服务器上，不会上传多余的cookie,但会多出额外的DNS解析时间
+ - 使用https代替http
+ - 清空无用的引入的文件，例如专注于写博客的话，其他所有和博客无关的东西都可以去除
+ - 国内打开github有时会很慢，无解问题
+
+
+
+这是之前修改landscape的[代码](https://github.com/kimown/blog/blob/d8e0a13f216495abb2a44c3433a6e72be0bb02ab/themes/auto-update-theme-fs.js),很槽糕的写法：代码重复度太高、可维护性极差、变量随意定义，只是单纯完成文件的替换功能，根本就没有考虑到以后扩展。
+
+新的思路:
+
+对于修改文件而言，主要两个参数：一是文件地址,统一下使用绝对路径，二是文件替换规则。替换规格可以利用string对象的[repalce()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace)方法，传入一个regexp或被替换的字符串，第二个参数可以是替换的字符串或函数。
+但是对于一个文本文件而言，没有办法精确到某一行，还需要结合文件的[换行符](https://nodejs.org/dist/latest-v4.x/docs/api/os.html#os_os)找到指定行，然后在用正则替换，现在linux，mac下是'\n'，window下是'\r\n'，[详情](https://www.zhihu.com/question/46542168)
+
+``` js
+var a = [{
+    filePath: '/tmp/a.js',
+    rules: [{line: 'number', old: 'reg or substr', new: 'newSubStr'}]
+}];
+JSON.stringify(a,null,2);
+```
+[JSON.stringify格式化对象为字符串方法](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify#The_space_argument)
+
+``` json
+[
+  {
+    "filePath": "/tmp/a.js",
+    "rules": [
+      {
+        "line": 233,
+        "old": "reg or substr",
+        "new": "newSubStr"
+      }
+    ]
+  }
+]
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- themes/auto-update-theme-fs.js
+++ themes/auto-update-theme-fs.js
@@ -11,126 +11,84 @@ const fs = require('fs');
 const path = require('path');
 const assert = require('assert');
 
-const themeDirName = process.argv[2] ? process.argv[2] : 'hexo-theme-landscape-tmp';
-
-const themeDirPath = path.join(__dirname, themeDirName);
-
-const cssDirPath = path.join(themeDirPath, 'source', 'css');
-
-const _partialPath=path.join(themeDirPath,'layout','_partial');
-
 
 process.on('message', function (m) {
     console.log("fs:" + m);
 });
 
-/**
- * modify   _variables.styl
- */
+// const themeDirName = process.argv[2] ? process.argv[2] : 'hexo-theme-landscape-tmp';
+const themeDirName = 'hexo-theme-landscape-tmp';
 
-let _variabesPath = path.join(cssDirPath, '_variables.styl');
-let _variablesOption = {
-    fileName: '_variables.styl',
-    soruceCode: fs.readFileSync(_variabesPath).toString(),
-    lineNumber: 9,
-    expectSourceCode: 'color-background = #eee',
-    myExpectSourceCode: 'color-background = #171f26'
-};
-let _variablesSourceCodeReplaceAfter = replaceWithSpecifyLine(_variablesOption);
-fs.writeFileSync(_variabesPath, _variablesSourceCodeReplaceAfter);
-
-
-// modify  _variables.styl  clear banner picture
-let _variablesBannerOption = {
-    fileName: '_variables.styl',
-    soruceCode: fs.readFileSync(_variabesPath).toString(),
-    lineNumber: 38,
-    expectSourceCode: 'banner-url = "images/banner.jpg"',
-    myExpectSourceCode: 'banner-url = ""'
+const filePaths = {
+    'cssPath': path.resolve(__dirname, themeDirName, 'source/css'),
+    '_partialPath': path.resolve(__dirname, themeDirName, 'layout/_partial')
 };
 
-let _variablesBannerSourceCodeReplaceAfter = replaceWithSpecifyLine(_variablesBannerOption);
-fs.writeFileSync(_variabesPath, _variablesBannerSourceCodeReplaceAfter);
+const modifyFileOption = [
+    {
+        "filePath": path.resolve(filePaths.cssPath, '_variables.styl'),
+        "rules": [
+            {
+                "line": 10,
+                "old": "color-background = #eee",
+                "new": "color-background = #171f26"
+            },
+            {
+                "line": 29,
+                "old": `font-icon-path = "fonts/fontawesome-webfont"`,
+                "new": `font-icon-path = "//cdn.bootcss.com/font-awesome/4.0.3/fonts/fontawesome-webfont"`
+            },
+            {
+                "line": 39,
+                "old": `banner-url = "images/banner.jpg"`,
+                "new": `banner-url = ""`
+            }
+        ]
+    },
+    {
+        "filePath": path.resolve(filePaths.cssPath, 'style.styl'),
+        "rules": [
+            {
+                "line": 65,
+                "old": "  background: color-background",
+                "new": ""
+            }
+        ]
+    },
+    {
+        "filePath": path.resolve(filePaths._partialPath, 'after-footer.ejs'),
+        "rules": [
+            {
+                "line": 17,
+                "old": `<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>`,
+                "new": `<script src="//staticfile.qnssl.com/jquery/2.0.3/jquery.min.js"></script>`
+            },
+            {
+                "line": 20,
+                "old": "'fancybox/jquery.fancybox'",
+                "new": "'//staticfile.qnssl.com/fancybox/2.1.5/jquery.fancybox'"
+            },
+            {
+                "line": 21,
+                "old": "'fancybox/jquery.fancybox.pack'",
+                "new": "'//staticfile.qnssl.com/fancybox/2.1.5/jquery.fancybox.pack'"
+            }
+        ]
+    },
+    {
+        "filePath": path.resolve(filePaths._partialPath, 'head.ejs'),
+        "rules": [
+            {
+                "line": 32,
+                "old": `fonts.googleapis.com`,
+                "new": `fonts.lug.ustc.edu.cn`
+            }
+        ]
+    }
+];
 
 
-let _fontawesome_webfontOption={
-    fileName: '_variables.styl',
-    soruceCode: fs.readFileSync(_variabesPath).toString(),
-    lineNumber: 28,
-    expectSourceCode: 'font-icon-path = "fonts/fontawesome-webfont"',
-    myExpectSourceCode: 'font-icon-path = "//cdn.bootcss.com/font-awesome/4.0.3/fonts/fontawesome-webfont"'
-};
-let _fontawesome_webfontSourceCodeReplaceAfter = replaceWithSpecifyLine(_fontawesome_webfontOption);
-fs.writeFileSync(_variabesPath, _fontawesome_webfontSourceCodeReplaceAfter);
-
-
-
-/**
- *
- * modify style.styl
- *
- */
-let stylePath = path.join(cssDirPath, 'style.styl');
-let styleOption = {
-    fileName: 'style.styl',
-    soruceCode: fs.readFileSync(stylePath).toString(),
-    lineNumber: 64,
-    expectSourceCode: '  background: color-background',
-    myExpectSourceCode: ''
-}
-
-let styleSourceCodeReplaceAfter = replaceWithSpecifyLine(styleOption);
-fs.writeFileSync(stylePath, styleSourceCodeReplaceAfter);
-
-
-
-
-
-
-
-
-
-/**
- * modify after-footer.ejs
- */
-let _afterFooterEjsPath = path.join(_partialPath, 'after-footer.ejs');
-let _afterFooterEjsOption = {
-    fileName: 'after-footer.ejs',
-    soruceCode: fs.readFileSync(_afterFooterEjsPath).toString(),
-    lineNumber: 16,
-    expectSourceCode: '<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>',
-    myExpectSourceCode: '<script src="//ajax.lug.ustc.edu.cn/ajax/libs/jquery/2.0.3/jquery.min.js"></script>'
-}
-
-let _afterFooterEjsSourceCodeReplaceAfter = replaceWithSpecifyLine(_afterFooterEjsOption);
-fs.writeFileSync(_afterFooterEjsPath, _afterFooterEjsSourceCodeReplaceAfter);
-
-
-/**
- *
- * modify header.ejs
- */
-let headerejsPath=path.join(_partialPath, 'head.ejs');
-let headerjsOption={
-    fileName: 'head.ejs',
-    soruceCode: fs.readFileSync(headerejsPath).toString(),
-    lineNumber: 31,
-    expectSourceCode: '    <link href="//fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet" type="text/css">',
-    myExpectSourceCode: '    <link href="//fonts.lug.ustc.edu.cn/css?family=Source+Code+Pro" rel="stylesheet" type="text/css">'
-};
-let headerejsSourceCodeReplaceAfter = replaceWithSpecifyLine(headerjsOption);
-fs.writeFileSync(headerejsPath, headerejsSourceCodeReplaceAfter);
-
-
-
-
-
-
-
-
-
-
-
+replaceWithSpecifyLine(modifyFileOption);
 
 
 console.log('change theme file successfully,exiting sub process!!');
@@ -142,17 +100,35 @@ process.exit(0);
  * only replace specify line
  * now,node 4.* did not support destructure
  */
-function replaceWithSpecifyLine(option) {
-    let {fileName,soruceCode,lineNumber,expectSourceCode,myExpectSourceCode }=option;
-    let sourceCodeAr = soruceCode.split('\n');
-    let soruceCodeSpecifyLine = sourceCodeAr[lineNumber];
+function replaceWithSpecifyLine(filesOption) {
     try {
-        assert.ok([expectSourceCode, myExpectSourceCode].includes(soruceCodeSpecifyLine),
-            `${fileName} encounter a problem!!WARNING: the old way changing code is outdated!!`
-        );
+
+        filesOption.map((fileOption)=> {
+            let {filePath, rules}=fileOption;
+            let fileName = filePath.split('/').pop();
+            let sourceCode = fs.readFileSync(filePath).toString();
+            let sourceCodeAr = sourceCode.split('\n');
+            rules.map((rule)=> {
+                let {line, old} = rule;
+                let now = rule['new'];
+
+                let soruceCodeSpecifyLine = sourceCodeAr[line - 1];
+                assert.ok(soruceCodeSpecifyLine.search(old) != -1,
+                    `${fileName} encounter a problem At ${line}
+                    I want to find ${old},
+                    but the file is ${soruceCodeSpecifyLine},
+                    !!WARNING: the old way changing code is outdated!!`
+                )
+                sourceCodeAr[line - 1] = soruceCodeSpecifyLine.replace(old, now);
+            })
+            let currentSourceCode = sourceCodeAr.join('\n');
+            fs.writeFileSync(filePath, currentSourceCode);
+        });
+
     } catch (e) {
+        console.error(e);
         process.send({'ok': false});
     }
-    sourceCodeAr[lineNumber] = myExpectSourceCode;
-    return sourceCodeAr.join('\n');
+
+
 }
